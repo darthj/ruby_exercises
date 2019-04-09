065b8ca0df9af56f2dadf43323505eb425752f1d
Jerald <darthj93@gmail.com>
2019-04-09 12:53:55 -0500

    update to times.rb

---
+++ times.rb
@@ -0,0 +1,48 @@
+# 5.times do 
+# 	puts "I think I can"
+# end
+
+# count = 0
+# 10.times do 
+# 	puts count*count
+# 	count += 1
+# end
+
+# num = 1
+# until num == 10
+#     puts num
+#     num += 1
+# end
+
+# puts "Please give me a number between 1 and 10"
+# num = gets.chomp.to_i
+
+# until num < 0
+# 	puts num
+# 	num -= 1
+# end
+
+# puts "can we go to itchy and scratchy land?"
+# answer = gets.chomp
+
+# until answer == "yes"
+# 	puts "can we go to itchy and scratchy land?"
+# 	answer = gets.chomp
+# end
+# puts "Hooray!!!"
+
+# puts "Please enter your first name"
+# name = gets.chomp.capitalize
+# while name != "Jacob"
+#     puts "Please enter your first name"
+# 	name = gets.chomp.capitalize
+# end
+
+num = rand(1..10)
+while num != 7
+	puts num
+	num = rand(1..10)
+end
+puts "you hit the jackpot!!!"
+
+
